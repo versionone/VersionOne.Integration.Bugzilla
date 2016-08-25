8bdcde3a2f838a9cda1b728387b9ddb311190931
darchitetto <denise@digital-pioneers.com>
2016-08-25 15:38:30 -0400

    S-51801 Add Bug Object

---
+++ VersionOne.Bugzilla.BugzillaAPI/Bug.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace VersionOne.Bugzilla.BugzillaAPI
+{
+	public class Bug
+	{
+		public string ID { get; set; }
+		public string Name { get; set; }
+		public string Priority { get; set; }
+		public string Product { get; set; }
+		public string ComponentID { get; set; }
+		public string AssignedTo { get; set; }
+	}
+}
