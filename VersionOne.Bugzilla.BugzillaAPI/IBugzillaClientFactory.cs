9de52c0907c57ca8602151640ef75bece15053f7
Cory Wheeler <corywheeler@gmail.com>
2016-10-19 16:00:17 -0400

    S-51801: Remove un-necessary using statements.

--- VersionOne.Bugzilla.BugzillaAPI/IBugzillaClientFactory.cs
+++ VersionOne.Bugzilla.BugzillaAPI/IBugzillaClientFactory.cs
@@ -1,6 +1,4 @@
-﻿using VersionOne.ServiceHost.Core.Logging;
-
-namespace VersionOne.Bugzilla.BugzillaAPI
+﻿namespace VersionOne.Bugzilla.BugzillaAPI
 {
     public interface IBugzillaClientFactory
     {
