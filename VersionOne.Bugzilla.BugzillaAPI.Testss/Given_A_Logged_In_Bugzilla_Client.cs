314e92318164b50bcd62f2b592ae5be4cea28749
Cory Wheeler <corywheeler@gmail.com>
2016-10-19 10:38:18 -0400

    S-51801: Rename the test class to have a Given BDD style class name and remove some of the tests that were not implemented because we realized we didn't need the behavior.

--- VersionOne.Bugzilla.BugzillaAPI.Testss/Given_A_Logged_In_Bugzilla_Client.cs
+++ VersionOne.Bugzilla.BugzillaAPI.Testss/Given_A_Logged_In_Bugzilla_Client.cs
@@ -7,7 +7,7 @@ using Rhino.Mocks;
 namespace VersionOne.Bugzilla.BugzillaAPI.Tests
 {
     [TestClass()]
-    public class BugzillaClientTest
+    public class Given_A_Logged_In_Bugzilla_Client
     {
 
         private IBugzillaClient _client;
@@ -57,7 +57,7 @@ namespace VersionOne.Bugzilla.BugzillaAPI.Tests
             var response = _client.Search(searchCriteria);
 
             var count = response.Count();
-            Assert.IsNotNull(count);
+            Assert.IsTrue(count > 0);
         }
 
         [TestMethod]
@@ -69,18 +69,8 @@ namespace VersionOne.Bugzilla.BugzillaAPI.Tests
             Assert.IsNotNull(bug);
         }
 
-        [TestMethod, Ignore]
-        public void when_calling_status_exists_it_should_return_true()
-        {
-            int ID = 7;
-            string status = "CONFIRMED";
-            //	bool exists = _client.StatusExists(status);
-
-            //	Assert.IsTrue(exists);
-        }
-
         [TestMethod]
-        public void when_calling_accept_bug_the_status_change()
+        public void when_calling_accept_bug_the_status_changes()
         {
             int ID = 7;
             string status = "IN_PROGRESS";
@@ -93,7 +83,7 @@ namespace VersionOne.Bugzilla.BugzillaAPI.Tests
 
         [TestMethod]
         [ExpectedException(typeof(Exception), "Bug #9999 does not exist.")]
-        public void when_calling_a_bug_that_dont_exist_it_should_throw_an_exception()
+        public void when_calling_a_bug_that_doesnt_exist_it_should_throw_an_exception()
         {
             int ID = 9999;
             Bug bug = _client.GetBug(ID);
@@ -127,28 +117,6 @@ namespace VersionOne.Bugzilla.BugzillaAPI.Tests
             Assert.IsTrue(comment == expectedComment);
         }
 
-
-        [TestMethod, Ignore]
-        public void when_calling_user_can_edit_the_user_should_be_able_to_do_it()
-        {
-            int ID = 7;
-
-            Bug bug = _client.GetBug(ID);
-
-            //  Assert.IsTrue(_client.UserCanEdit(bug));
-        }
-
-        [TestMethod, Ignore]
-        public void when_calling_find_product_it_should_return_product_id()
-        {
-            int ID = 7;
-            var product_id = 1;
-
-            Bug bug = _client.GetBug(ID);
-
-            //  Assert.AreEqual(_client.findProductId(bug), product_id);
-        }
-
         [TestMethod]
         public void when_calling_reassign_bug_it_should_change_the_assigned_bug()
         {
@@ -163,22 +131,6 @@ namespace VersionOne.Bugzilla.BugzillaAPI.Tests
             Assert.AreEqual(bug.AssignedTo, AssignedToUser);
         }
 
-
-        [TestMethod]
-        public void when_calling_accept_bug_it_should_change_the_status()
-        {
-            int ID = 25;
-
-            var newBugStatus = "IN_PROGRESS";
-
-            _client.AcceptBug(ID, newBugStatus);
-
-            Bug bug = _client.GetBug(ID);
-
-            Assert.AreEqual(bug.Status, newBugStatus);
-        }
-
-        
         [TestMethod]
         public void when_udpating_a_bug_field_the_field_gets_its_new_value()
         {
@@ -193,18 +145,6 @@ namespace VersionOne.Bugzilla.BugzillaAPI.Tests
             var bug = _client.GetBug(bugId);
 
             Assert.AreEqual(expectedValue, bug.Name);
-        }
-
-        [TestMethod]
-        public void when_serching_for_bugs_assigned_to_integration_user_we_should_get_a_list_of_ids_as_ints()
-        {
-            var queryString = "email1=terry.densmore%40versionone.com&emailassigned_to1=1&emailtype1=equals&query_format=advanced&resolution=---&known_name=Assigned%20Bugs";
-            
-            var ids =  _client.Search(queryString);
-
-            var firstId = ids.First();
-
-            Assert.IsTrue(ids.Count > 0);
         }
     }
 }
