b41726668440d58c48f38ae623c94711cc9cf980
Cory Wheeler <corywheeler@gmail.com>
2016-10-21 15:13:08 -0400

    S-54454: Change formatting to match C# style

--- VersionOne.ServiceHost.ConfigurationTool/Entities/VersionOneSettings.cs
+++ VersionOne.ServiceHost.ConfigurationTool/Entities/VersionOneSettings.cs
@@ -2,28 +2,32 @@ using System.Xml.Serialization;
 using VersionOne.ServiceHost.ConfigurationTool.Validation;
 using VersionOne.ServiceHost.ConfigurationTool.Attributes;
 
-namespace VersionOne.ServiceHost.ConfigurationTool.Entities {
+namespace VersionOne.ServiceHost.ConfigurationTool.Entities
+{
     /// <summary>
     /// VersionOne connection settings node backing class.
     /// </summary>
     [XmlRoot("Settings")]
-    public class VersionOneSettings {
+    public class VersionOneSettings
+    {
         public const string ApplicationUrlProperty = "ApplicationUrl";
         public const string UsernameProperty = "Username";
         public const string PasswordProperty = "Password";
         public const string IntegratedAuthProperty = "IntegratedAuth";
 
-        public VersionOneSettings() {
+        public VersionOneSettings()
+        {
             ProxySettings = new ProxyConnectionSettings();
         }
 
         [XmlElement("APIVersion")]
-        public string ApiVersion {
+        public string ApiVersion
+        {
             get { return "7.2.0.0"; }
             set { }
         }
 
-        [HelpString(HelpResourceKey="V1PageVersionOneUrl")]
+        [HelpString(HelpResourceKey = "V1PageVersionOneUrl")]
         [NonEmptyStringValidator]
         public string ApplicationUrl { get; set; }
 
@@ -33,7 +37,7 @@ namespace VersionOne.ServiceHost.ConfigurationTool.Entities {
         [NonEmptyStringValidator]
         public string Password { get; set; }
 
-        [HelpString(HelpResourceKey="V1PageIntegratedAuth")]
+        [HelpString(HelpResourceKey = "V1PageIntegratedAuth")]
         public bool IntegratedAuth { get; set; }
 
         public ProxyConnectionSettings ProxySettings { get; set; }
